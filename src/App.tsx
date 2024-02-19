9d9df218afa63abf1324d49ef58f278ad7350042
Bochkovskyi <unundertale1@ukr.net>
2024-02-19 14:50:12 +0200

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { FC } from 'react'
+
+const App: FC = () => {
+  return (
+    <div className="mx-auto flex h-screen max-w-prose items-center justify-center text-center text-2xl">
+      React + TypeScript + TailwindCSS starting template
+    </div>
+  )
+}
+
+export default App
